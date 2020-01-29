ee4638ad70afc188641b3d41b682921c87726fd4
Nobeul <nobeul.cse@gmail.com>
2020-01-29 11:02:22 +0600

    added append functionality for add option

--- Json-ldForSchema/usingPostVariableAsArray.php
+++ Json-ldForSchema/usingPostVariableAsArray.php
@@ -29,6 +29,8 @@
 //     );
 //     $encodedValue =  json_encode($action);
 // }
+
+// echo ++$abc;
 echo json_encode($_POST);
 
 ?>
@@ -40,8 +42,7 @@ echo json_encode($_POST);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
-        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous" />
     <title>Json testing </title>
 </head>
 
@@ -96,32 +97,32 @@ echo json_encode($_POST);
         </div>
     </div>
 
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
-        integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous">
     </script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
-        integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous">
     </script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
-        integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">
     </script>
 
 
     <script>
-    $("#add").click(function(event) {
-        event.preventDefault();
+        $("#add").click(function(event) {
+            event.preventDefault();
+            $("#addOrRemove").append(
+                '<label for="exampleInputabc">abc</label><input type="text" class="form-control" name="<?php echo 'abc' . $abc += 1; ?>"</span>'
+            );
 
-        $("#addOrRemove").append(
-            ' <label for="exampleInputphone">Participant</label><input type="text" class="form-control" name="participant" placeholder="Enter participant name"><label for="exampleInputphone">abc</label><input type="text" class="form-control" name="participant" placeholder="Enter participant name"><span id="phoneError" style="color: red;"></span>'
-        );
 
-    });
-    $("#remove").click(function(event) {
-        event.preventDefault();
 
-        $("#addOrRemove").remove();
-        // Add logic for single item remove from the last 
-    });
+            // first set a checking value limit. Then with the limit generate the append and use name field to the php. Then make child and parent hierarchy.
+
+        });
+        $("#remove").click(function(event) {
+            event.preventDefault();
+
+            $("#addOrRemove").remove();
+            // Add logic for single item remove from the last 
+        });
     </script>
 
 
